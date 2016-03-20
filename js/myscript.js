ae25a6229554a8c99415d1c2a9b01cc2f69644c7
rhoak <rachaelhoak@gmail.com>
2016-03-20 12:42:19 -0400

    adding myscripts

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+console.log('foo');
+
+
+function printNumbers() {
+  for (i=1; i<=1000; i++) { 
+    console.log(i);
+  }
+}
+
+printNumbers();
--- server/server.js
+++ server/server.js
@@ -10,24 +10,4 @@ http.createServer((req, res) => {
 }).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-
-http.createServer((req, res) => {
-  //attach listener 
-  req.on("end", function () {
-    //the book
-    var book = req('/bookproc/testdata/gullivers_travels.txt');
-    //read the book file
-    fs.readFile(book, 'utf-8', function(error, data) {
-      res.writeHead(200, {
-        'Content-Type' : 'text/plain'
-      });
-      //find the A in book.
-      data = book.indexOf('a');
-      //Write to file the number of As
-      fs.writeFile(book, data);
-      //end reponse
-      res.end('This book contains' + data + 'incidents of the letter A!');
-    });
-  });
-}).listen(8080);
+ 
